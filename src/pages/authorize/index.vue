b44313544b4222df52797c0664faf842a3b1cb2f
huzhifu <2474016183@qq.com>
2018-07-25 03:22:35 +0800

    '授权基本逻辑'

---
+++ src/pages/authorize/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="authorize-warp">
+    <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="onGotUserInfo">获取用户信息</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  created () {
+
+  },
+  methods: {
+    onGotUserInfo: function (e) {
+      console.log(e.target.userInfo)
+      wx.navigateBack({
+        delta: 1
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.authorize-warp {
+  text-align: center;
+  padding-top: 100rpx;
+}
+
+</style>
